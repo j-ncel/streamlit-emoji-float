6dac06d95ce31c870afe9f90c7455743be492f40
j-ncel <juncel.datinggaling@gmail.com>
2025-11-22 22:59:12 +0800

    update: demo.py, modify emoji list for demo

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import streamlit as st
 from streamlit_emoji_float import emoji_float
 
-value = emoji_float(emojis=["⭐", "😊", "🎈"], count=1000)
+value = emoji_float(emojis=list("🎈😊⭐🤩✨😆"))
+print(value)
